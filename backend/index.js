afbff0e906da3712e1218e9204ea2a4777fe9ada
Nevohteeb <ciaranslow@outlook.com>
2023-02-27 09:49:08 +1300

    delete products, add users, loign endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const app = express(); // variable of app to us express method
 const cors = require('cors'); // bring in CORS
 const bodyParser = require('body-parser') // include BodyParser 
 const mongoose = require('mongoose') // import mongoose
+const bcrypt = require('bcryptjs');
 const config = require('./config.json'); // get config
 const Product = require('./models/product');
+const User = require('./models/user');
 
 const port = 8080; // set the port number for our local server
 
@@ -12,6 +14,11 @@ app.use((req, res, next) => {
     console.log(`${req.method} request ${req.url}`);
     next();
 })
+
+app.use(bodyParser.json()); // calling Body Parser method
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(cors()); // calling cors method with express
 
@@ -36,3 +43,98 @@ app.get('/allProductsFromDB', (req, res) => {
         res.send(result)
     })
 })
+
+// Post Method to CREATE a product
+app.post('/addProduct', (req, res) => {
+    const dbProduct = new Product({
+      _id: new mongoose.Types.ObjectId,
+      name: req.body.name,
+      price: req.body.price,
+      image_url: req.body.image_url
+    });
+    //save to the database and notify the user
+    dbProduct.save().then(result => {
+      res.send(result);
+    }).catch(err => res.send(err));
+})
+
+// Edit/ UPDATE using 'PATCH' http method
+app.patch('/updateProduct/:id', (req, res) => {
+    const idParam = req.params.id;
+    Product.findById(idParam, (err, product) => {
+        const updatedProduct = {
+            name: req.body.name,
+            price: req.body.price,
+            image_url: req.body.image_url 
+        }
+        Product.updateOne({
+            _id: idParam
+        }, updatedProduct).
+        then(result => {
+            res.send(result);
+        }).catch(err => res.send(err))
+    })
+})
+
+app.delete('/deleteProduct/:id', (req,res) => {
+    const idParam = req.params.id;
+    Product.findOne({
+        _id: idParam
+    }, (err, product) => {
+        if (product) {
+            Product.deleteOne({
+                _id: idParam
+            }, err => {
+                console.log('deleted on backend request');
+            });
+        } else {
+            alert('not found');
+        }
+    }).catch(err => res.send(err));
+}); // Delete
+
+// ----------------------- USER END POINTS -----------------------
+
+// REGISTER USER
+app.post('/registerUSer', (req,res) => {
+    User.findOne({
+        username: req.body.username,
+    }, (err, userExists) => {
+        if (userExists) {
+            res.send('username already taken');
+        } else {
+            const hash = bcrypt.hashSync(req.body.password);
+            const user = new User({
+                _id: new mongoose.Types.ObjectId,
+                username: req.body.username,
+                password: hash,
+                email: req.body.email
+            });
+            user.save()
+                .then(result => {
+                    console.log(user, result);
+                    res.send(result);
+                }).catch(err => {
+                    res.send(err)
+                });
+        }//else
+    })// findone
+}); // end of register
+
+// LOGIN USER
+app.post('/loginUser', (req, res) => {
+    User.findOne({username:req.body.username}, (err, userResult) => {
+        if (userResult) {
+            if (bcrypt.compareSync(req.body.password, userResult.password)) {
+                res.send(userResult);
+            } else {
+                res.send('not authorized');
+            }// inner if
+        } else {
+            res.send('user not found. Please Register')
+        }// outer if 
+    }); //find one
+}); // end of post for login
+
+
+
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */ 
+
 console.log('frontend script is working');
 
 $(document).ready(function(){
@@ -17,7 +19,7 @@ $(document).ready(function(){
         error: function(error){
             console.log(error);
         }
-    })
+    });
 
     //View Products onclick of View Products Button
     $('#viewProducts').click(function(){
@@ -26,14 +28,147 @@ $(document).ready(function(){
             type: 'GET',
             dataType: 'json',
             success: function(productsFromMongo) {
+                document.getElementById('result').innerHTML = '';
                 for(let i = 0; i < productsFromMongo.length; i++ ){
                     console.log(productsFromMongo[i]);
+                    document.getElementById('result').innerHTML += `
+                    <div class="col-4 mt-3 mb-3">
+                        <div class="card">
+                            <img class="card-img-top" src="${productsFromMongo[i].image_url}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${productsFromMongo[i].name}</h5>
+                                 <p class="card-text">${productsFromMongo[i].price}</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    `;
                 }
             },
             error: function() {
-                alert('unable to get products')
+                alert('unable to get products');
             }
-        })
-    })
+        });
+    });// End of View Products
 
-}) // Doc Ready function Ends
+    //add a product form a click
+    $('#addProduct').click(function(event){
+        event.preventDefault();
+        let name = $('#a-name').val();
+        let price = $('#a-price').val();
+        let image_url = $('#a-imageurl').val();
+        console.log(name,price, image_url);
+        if (name == '' || price == '' || image_url == ''){
+        alert('Please login and enter all details');
+        } else {
+        $.ajax({
+            url : `http://${url}/addProduct`,
+            type : 'POST',
+            data :{
+            name: name,
+            price: price,
+            image_url:image_url,
+            },
+            success : function(product){
+            console.log(product);
+            alert ('product added');
+            },
+            error : function(){
+            console.log('error: cannot call api');
+            }//error
+          });//ajax
+        }//else
+    });//addProduct Click
+
+    $('#updateProduct').click(function(event){
+        event.preventDefault();
+        let productId = $('#productId').val();
+        let productName = $('#productName').val();
+        let productPrice = $('#productPrice').val();
+        let productImageUrl = $('#imageurl').val();
+        console.log(productId, productName, productPrice, productImageUrl);
+        if (productId == '') {
+            alert('Please enter a product to update');
+        } else {
+            $.ajax({
+                url: `http://${url}/updateProduct/${productId}`,
+                type: 'PATCH',
+                data: {
+                    name: productName,
+                    price: productPrice,
+                    image_url: productImageUrl
+                },
+                success: function(data){
+                    console.log(data);
+                },
+                error: function(){
+                    console.log('error: cannot update post');
+                }// error
+            }); // ajax
+        }// if
+    });// update click
+
+    // DELETE PRODUCT
+    $('#deleteProduct').click(function(event){
+        event.preventDefault();
+        let productId =$('#delProductId').val();
+        console.log(productId);
+        if (productId == '') {
+            alert('Please eneter product id to delete');
+        } else {
+            $.ajax({
+                url: `http://${url}/deleteProduct/${productId}`,
+                type: 'DELETE',
+                success: function() {
+                    console.log('deleted');
+                    alert('Product Deleted');
+                },
+                error: function() {
+                    console.log('error: cannot delete due to call on api');
+                }// error
+            }); // ajax
+        }// if
+    }); // delete product
+
+
+    // ---------------------- ADD USER API CALLS -------------------
+
+    // Register User
+    $('#r-submit').click(function (event) {
+        event.preventDefault();
+        let username =$('#r-username').val();
+        let email =$('#r-email').val();
+        let password =$('#r-password').val();
+        console.log(username, email, password);
+
+        if (username == '' || email == '' || password == '') {
+            alert('Please enter all details');
+        } else {
+            $.ajax({
+                url: `http://${url}/registerUser`,
+                type: 'POST',
+                data: {
+                    username: username,
+                    email: email,
+                    password: password
+                },
+                success: function(user) {
+                    console.log(user); // remove when dev is finished
+                    if (user !== 'username already taken'){
+                        alert('Thank you for registering')
+                    } else {
+                        alert('Username taken already. Please try again');
+                        $('#r-username').val('');
+                        $('#r-email').val('');
+                        $('#r-password').val('');
+                    } // else
+                },
+                error: function() {
+                    console.log('error: cannot call add user api');
+                }// error
+            }); // end of ajax
+        } // end of else
+    }); // end of submit user click
+
+
+}); // Doc Ready function Ends
